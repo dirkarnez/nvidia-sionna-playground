901cfff8243b844125ba239bbb0ee6c92866c1f4
dirkarnez <smalldirkalex@gmail.com>
2026-09-18 18:14:48 +0800

    - wip

--- ray-tracing/main_1.py
+++ ray-tracing/main_1.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+import numpy as np
 from sionna.rt import load_scene, Transmitter, Receiver, scene, PathSolver, PlanarArray
 
 # Load integrated scene
@@ -25,10 +27,30 @@ paths = p_solver(scene, los=True, specular_reflection=True,
 # scene.render(paths=paths, coverage_map=cm)
 
 # Convert paths to channel impulse responses
-a_rt, _ = paths.cir(sampling_frequency=1e6)
+# a_rt, _ = paths.cir(sampling_frequency=1e6)
 
-a = paths.a[0].numpy() + 1j*paths.a[1].numpy()
-tau = paths.tau.numpy()
-doppler = paths.doppler.numpy()
-print(a)
-print(tau)
+# a = paths.a[0].numpy() + 1j*paths.a[1].numpy()
+# tau = paths.tau.numpy()
+# doppler = paths.doppler.numpy()
+# print(a)
+# print(tau)
+
+
+a, tau = paths.cir(normalize_delays=True, out_type="numpy")
+
+# Shape: [num_rx, num_rx_ant, num_tx, num_tx_ant, num_paths, num_time_steps]
+print("Shape of a: ", a.shape)
+
+# Shape: [num_rx, num_rx_ant, num_tx, num_tx_ant, num_paths]
+print("Shape of tau: ", tau.shape)
+
+t = tau[0,0,:]/1e-9 # Scale to ns
+a_abs = np.abs(a)[0,0,:,0]
+a_max = np.max(a_abs)
+
+# And plot the CIR
+plt.figure()
+plt.title("Channel impulse response")
+plt.stem(t, a_abs)
+plt.xlabel(r"$\tau$ [ns]")
+plt.ylabel(r"$|a|$");
